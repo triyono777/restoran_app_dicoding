298afb7e550f7e5c72714d92d048044ac149c0f2
triyono777 <yono.tri>
2020-10-29 10:36:24 +0700

    add model, add list

---
+++ lib/ui/page/home_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  static const routeName = '/Homepage';
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('List Restaurant'),
+      ),
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (ctx, index) => ListTile(
+          title: Text('Nama Restaurant'),
+        ),
+      ),
+    );
+  }
+}
