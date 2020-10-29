5315d7bf3364316f4eb3fd4d32d3b984689c2f45
triyono <yono.tri@gmail.com>
2020-10-30 02:22:58 +0700

    editing search

--- lib/ui/page/home_page.dart
+++ lib/ui/page/home_page.dart
@@ -4,8 +4,21 @@ import 'package:restoran_app_dicoding/model/restaurant_model.dart';
 import 'package:restoran_app_dicoding/ui/page/detail_restaurat.dart';
 import 'package:restoran_app_dicoding/ui/widgets/item_list_restaurant.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   static const routeName = '/Homepage';
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Future<RestaurantModel> future;
+  TextEditingController search;
+  @override
+  void initState() {
+    super.initState();
+    future = RestaurantController().getRestaurant(context);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,24 +30,26 @@ class HomePage extends StatelessWidget {
             buildTitle(),
             Expanded(
               child: FutureBuilder<RestaurantModel>(
-                future: RestaurantController().getRestaurant(context),
+                future: future,
                 builder: (ctx, snapshot) => snapshot.connectionState == ConnectionState.waiting
                     ? Center(child: CircularProgressIndicator())
                     : snapshot.hasError
                         ? Text('terjadi kesalahan load data')
                         : ListView.builder(
                             itemCount: snapshot.data.restaurants.length,
-                            itemBuilder: (ctx, index) => GestureDetector(
-                              onTap: () {
-                                Navigator.of(context).pushNamed(
-                                  DetailRestaurantPage.routeName,
-                                  arguments: snapshot.data.restaurants[index],
-                                );
-                              },
-                              child: ItemListRestaurant(
-                                restaurants: snapshot.data.restaurants[index],
-                              ),
-                            ),
+                            itemBuilder: (ctx, index) => snapshot.data.restaurants[index].name.contains(search.text)
+                                ? GestureDetector(
+                                    onTap: () {
+                                      Navigator.of(context).pushNamed(
+                                        DetailRestaurantPage.routeName,
+                                        arguments: snapshot.data.restaurants[index],
+                                      );
+                                    },
+                                    child: ItemListRestaurant(
+                                      restaurants: snapshot.data.restaurants[index],
+                                    ),
+                                  )
+                                : Container(),
                           ),
               ),
             ),
@@ -47,14 +62,37 @@ class HomePage extends StatelessWidget {
   Widget buildTitle() {
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Text(
-            'Restaurant',
-            style: TextStyle(fontSize: 30),
+//          Column(
+////            crossAxisAlignment: CrossAxisAlignment.start,
+////            children: [
+////              Text(
+////                'Restaurant',
+////                style: TextStyle(fontSize: 30),
+////              ),
+////              Text('Recomended Restaurants for you'),
+////            ],
+////          ),
+
+          Expanded(
+            child: TextField(
+              controller: search,
+              decoration: InputDecoration(
+                hintText: 'Search Restaurant',
+                border: UnderlineInputBorder(),
+              ),
+            ),
           ),
-          Text('Recomended Restaurants for you'),
+          IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {
+                setState(() {
+                  future.then((value) => value.restaurants.where((element) => element.name == search.text).toList());
+                  print('oke');
+                });
+              }),
         ],
       ),
     );
